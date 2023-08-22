5f2ae9f0c0349eabca85a1400c9b79edef20540e
phamtuan19 <phamtuan19hd@gmail.com>
2023-08-22 09:26:09 +0700

    config deploy vercel

--- src/database/connect.js
+++ src/database/connect.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 dotenv.config();
 
 const DB_Name = process.env.DB_Name;
-const DB_URL = `mongodb+srv://phamtuan19hd:phamtuan1982000qwer@cluster0.4mddcjn.mongodb.net/?retryWrites=true&w=majority`;
+const DB_URL = `mongodb://127.0.0.1:27017/${DB_Name}`;
 
 export const connectDB = async () => {
     try {
