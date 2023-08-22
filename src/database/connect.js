a46d1d1ee599ee6da63b091b32a7108e3ffb22de
phamtuan19 <phamtuan19hd@gmail.com>
2023-08-22 11:18:45 +0700

    e

--- src/database/connect.js
+++ src/database/connect.js
@@ -4,8 +4,8 @@ import mongoose from 'mongoose';
 dotenv.config();
 
 const DB_Name = process.env.DB_Name;
-// const DB_URL = `mongodb://127.0.0.1:27017/${DB_Name}`;
-const DB_URL = `mongodb+srv://phamtuan19hd:phamtuan1982000qwer@cluster0.4mddcjn.mongodb.net/?retryWrites=true&w=majority`;
+const DB_URL = `mongodb://127.0.0.1:27017/${DB_Name}`;
+// const DB_URL = `mongodb+srv://phamtuan19hd:phamtuan1982000qwer@cluster0.4mddcjn.mongodb.net/?retryWrites=true&w=majority`;
 
 export const connectDB = async () => {
     try {
